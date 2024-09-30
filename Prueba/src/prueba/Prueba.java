dd02b95d2403892e5041dcb99bfc8653e956f6e0
kevin <kevin@Kevin>
2024-09-30 13:06:54 +0200

    PrimerCommmit

--- Prueba/src/prueba/Prueba.java
+++ Prueba/src/prueba/Prueba.java
@@ -16,7 +16,7 @@ public class Prueba {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        //HOLA ESTO ES UNA PRUEBA
+        //
         
     }
     
